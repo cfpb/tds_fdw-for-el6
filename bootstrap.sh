2b30b896fea950d9fb1cf1f6cb9a67477860fe9a
olivermel <attybee10@yahoo.com>
2016-07-27 11:28:02 -0400

    fixed typo

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
 sudo yum -y groupinstall 'Development Tools'
-sudo yum -y install freetds freetds-devel tree vim
-wget https://download.postgresql.org/pub/repos/yum/9.5/redhat/rhel-7-x86_64/pgdg-centos95-9.5-2.noarch.rpm
-sudo rpm -ivh pgdg-centos95-9.5-2.noarch.rpm
-sudo yum install postgresql95 postgresql95-server postgresql95-libs postgresql95-devel
 
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
